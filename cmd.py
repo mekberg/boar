3830c4b8eee9d2a32bd5b39d525f8b5884c70819
Mats Ekberg <ekberg@gmail.com>
2009-09-23 19:34:35 +0000

    Funkar

--- cmd.py
+++ cmd.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-
+from __future__ import with_statement
 import sys
 import repository
+import storage
+import os
 
 def print_help():
     print """Usage: 
@@ -13,7 +15,29 @@ mkrepo <dir to create>
 
 
 def cmd_ci(args):
-    pass
+    repopath = os.getenv("REPO_PATH")
+    if repopath == None:
+        print "You need to set REPO_PATH"
+        return
+    repo = repository.Repo(repopath)
+    path_to_ci = args[0]
+    assert os.path.exists(path_to_ci)
+    s = storage.RepoWriter(repo)
+    s.new_session("TestSession")
+    def visitor(arg, dirname, names):
+        for name in names:
+            print "Visiting", dirname, name
+            full_path = os.path.join(dirname, name)
+            if not os.path.isfile(full_path):
+                continue
+            with open(full_path, "r") as f:
+                data = f.read()
+            s.add(data, {"filename": full_path})
+        
+    os.path.walk(path_to_ci, visitor, None)
+
+    s.close_session()
+
 
 def cmd_mkrepo(args):
     repository.create_repository(args[0])
