695e4f40e28c982cc0bb649a4ae5d019f7a28bf3
Mats Ekberg <ekberg@gmail.com>
2009-09-23 12:10:14 +0000

    Extracted repository operations from session operations

--- storage.py
+++ storage.py
@@ -7,67 +7,30 @@ import tempfile
 import re
 import simplejson as json
 
+import repository
+
 def md5sum(data):
     m = md5.new()
     m.update(data)
     return m.hexdigest()
 
-def get_session_info(repopath, session_id):
-    session_path = get_session_path(repopath, session_id)
-    
-
-def get_session_path(repopath, session_id):
-    return os.path.join(repopath, str(session_id))
-
-def get_blob_path(repopath, sum):
-    os.path.join(session_path, sum[0:2], sum)
-
-def find_blob(repopath, sum):
-    """ Takes a md5sum arg as a string, and returns the path to the blob
-    with that checksum, if it exists """
-    for session_id in get_all_sessions(repopath):
-        session_path = get_session_path(repopath, session_id)
-        blob_path = os.path.join(session_path, sum)
-        if os.path.exists(blob_path):
-            return blob_path
-    return None
-
-def get_all_sessions(repopath):
-    session_dirs = []
-    for dir in os.listdir(repopath):
-        if re.match("^[0-9]+$", dir) != None:
-            session_dirs.append(int(dir))
-    return session_dirs
-
-def find_next_session_id(repopath):
-    assert os.path.exists(repopath)
-    session_dirs = get_all_sessions(repopath)
-    session_dirs.append(-1)
-    return max(session_dirs) + 1            
-
-def find_session(repopath, key, value):
-    pass
-    #for session_id in get_all_sessions(repopath):
-        #with open("")
-        #json.loads()
-
 class RepoWriter:
-    def __init__(self):
-        self.repopath = "/tmp/REPO"
+    def __init__(self, repo):
+        self.repo = repo
         self.session_path = None
         self.metadatas = []
         self.sessioninfo = {}
 
     def new_session(self, session_name):
         assert self.session_path == None
-        assert os.path.exists(self.repopath)
-        self.session_path = tempfile.mkdtemp(prefix = "tmp_", dir = self.repopath) 
+        assert os.path.exists(self.repo.repopath)
+        self.session_path = tempfile.mkdtemp(prefix = "tmp_", dir = self.repo.repopath) 
 
     def add(self, data, metadata = {}):
         assert self.session_path != None
         sum = md5sum(data)
         metadata["md5sum"] = sum
-        existing_blob = find_blob(self.repopath, sum)
+        existing_blob = self.repo.find_blob(sum)
         fname = os.path.join(self.session_path, sum)
         if not existing_blob and not os.path.exists(fname):
             f = open(fname, "w")
@@ -92,8 +55,8 @@ class RepoWriter:
         with open(session_filename, "w") as f:
             json.dump(self.sessioninfo, f, indent = 4)
 
-        fileno = find_next_session_id(self.repopath)
-        final_session_dir = os.path.join(self.repopath, str(fileno))
+        fileno = self.repo.find_next_session_id()
+        final_session_dir = os.path.join(self.repo.repopath, str(fileno))
         assert not os.path.exists(final_session_dir)
         os.rename(self.session_path, final_session_dir)
 
@@ -118,7 +81,8 @@ class RepoReader:
 
 
 def main():
-    s = RepoWriter()
+    r = repository.Repo("/tmp/REPO")
+    s = RepoWriter(r)
     s.new_session("TestSession")
     os.chdir("/tmp")
     def visitor(arg, dirname, names):
