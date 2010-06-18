6e771c2939528e1408a75828ed66a108b6810494
Mats Ekberg <ekberg@gmail.com>
2010-06-18 12:46:55 +0000

    More tests

--- tests/test_workdir.py
+++ tests/test_workdir.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import sys, os, unittest, tempfile, shutil
 from copy import copy
 
@@ -18,20 +19,32 @@ class TestWorkdir(unittest.TestCase):
     def setUp(self):
         self.workdir = tempfile.mktemp(prefix='workdir_', dir=TMPDIR)
         self.repopath = tempfile.mktemp(prefix='workdir_repo_', dir=TMPDIR)
-        os.mkdir(self.workdir)
         repository.create_repository(self.repopath)
+        #os.mkdir(self.workdir)
+        #self.wd = workdir.Workdir(self.repopath, "TestSession", None, self.workdir)
+
+    def initWorkdir(self):
+        os.mkdir(self.workdir)
         self.wd = workdir.Workdir(self.repopath, "TestSession", None, self.workdir)
+        self.wd.checkin()
 
     def tearDown(self):
         shutil.rmtree(self.workdir, ignore_errors = True)
         shutil.rmtree(self.repopath, ignore_errors = True)
 
     def testEmpty(self):
-        self.wd.checkin()
+        self.initWorkdir()
+        changes = self.wd.get_changes()
+        self.assertEqual(changes, ([], [], [], [], []))
 
     def testSimpleCheckin(self):
+        self.initWorkdir()
         self.addWorkdirFile("tjosan.txt", "tjosanhejsan")
+        changes = self.wd.get_changes()
+        self.assertEqual(changes, ([], ["tjosan.txt"], [], [], []))
         self.wd.checkin()
+        changes = self.wd.get_changes()
+        self.assertEqual(changes, (["tjosan.txt"], [], [], [], []))
 
     def addWorkdirFile(self, path, content):
         filepath = os.path.join(self.workdir, path)
