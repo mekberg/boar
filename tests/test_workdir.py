25c6e810be2bb1c792f08fd7614993e8f94e77f2
Mats Ekberg <ekberg@gmail.com>
2010-06-21 13:08:39 +0000

    Reorganized tests

--- tests/test_workdir.py
+++ tests/test_workdir.py
@@ -20,10 +20,6 @@ class TestWorkdir(unittest.TestCase):
         self.workdir = tempfile.mktemp(prefix='workdir_', dir=TMPDIR)
         self.repopath = tempfile.mktemp(prefix='workdir_repo_', dir=TMPDIR)
         repository.create_repository(self.repopath)
-        #os.mkdir(self.workdir)
-        #self.wd = workdir.Workdir(self.repopath, "TestSession", None, self.workdir)
-
-    def initWorkdir(self):
         os.mkdir(self.workdir)
         self.wd = workdir.Workdir(self.repopath, "TestSession", None, self.workdir)
         self.wd.checkin()
@@ -32,24 +28,42 @@ class TestWorkdir(unittest.TestCase):
         shutil.rmtree(self.workdir, ignore_errors = True)
         shutil.rmtree(self.repopath, ignore_errors = True)
 
-    def testEmpty(self):
-        self.initWorkdir()
-        changes = self.wd.get_changes()
-        self.assertEqual(changes, ([], [], [], [], []))
-
-    def testSimpleCheckin(self):
-        self.initWorkdir()
-        self.addWorkdirFile("tjosan.txt", "tjosanhejsan")
-        changes = self.wd.get_changes()
-        self.assertEqual(changes, ([], ["tjosan.txt"], [], [], []))
-        self.wd.checkin()
-        changes = self.wd.get_changes()
-        self.assertEqual(changes, (["tjosan.txt"], [], [], [], []))
-
     def addWorkdirFile(self, path, content):
         filepath = os.path.join(self.workdir, path)
         with open(filepath, "w") as f:
             f.write(content)
+    
+    def rmWorkdirFile(self, path):
+        filepath = os.path.join(self.workdir, path)
+        os.unlink(filepath)
+
+    #
+    # Actual tests start here
+    #
+
+    def testEmpty(self):
+        changes = self.wd.get_changes()
+        self.assertEqual(changes, ([], [], [], [], []))
+
+    def testGetChangesUnversionedFile(self):
+        # Test unversioned file
+        self.addWorkdirFile("tjosan.txt", "tjosanhejsan")
+        changes = self.wd.get_changes()
+        self.assertEqual(changes, ([], ["tjosan.txt"], [], [], []))
+
+    def testGetChangesUnchangedFile(self):        
+        self.addWorkdirFile("tjosan.txt", "tjosanhejsan")
+        self.wd.checkin()
+        changes = self.wd.get_changes()
+        self.assertEqual(changes, (["tjosan.txt"], [], [], [], []))
+
+    def testGetChangesMissingFile(self):
+        self.addWorkdirFile("tjosan.txt", "tjosanhejsan")
+        self.wd.checkin()
+        self.rmWorkdirFile("tjosan.txt")
+        changes = self.wd.get_changes()
+        self.assertEqual(changes, ([], [], [], ["tjosan.txt"], []))
+
 
 if __name__ == '__main__':
     unittest.main()
