35efda6ed51ec13fc59b424cbd7f4f5d95ba563d
Mats Ekberg <ekberg@gmail.com>
2010-09-30 12:02:10 +0000

    Fixed performance problem

--- macrotests/macrotest.sh
+++ macrotests/macrotest.sh
@@ -25,7 +25,7 @@ md5sum -c test_tree.md5 || { echo "Test tree failed md5 after unmodified check-i
 echo Test adding files to a base session
 rm -r test_tree || { echo "Couldn't remove test tree"; exit 1; }
 tar -xvzf test_tree_addition.tar.gz || { echo "Couldn't create test tree for addition"; exit 1; }
-REPO_PATH=$REPO $CMD import -u test_tree MyTestSession || { echo "Couldn't import added files"; exit 1; }
+REPO_PATH=$REPO $CMD import test_tree MyTestSession || { echo "Couldn't import added files"; exit 1; }
 rm -r test_tree || { echo "Couldn't remove test tree"; exit 1; }
 REPO_PATH=$REPO $CMD co MyTestSession test_tree || { echo "Couldn't check out tree"; exit 1; }
 md5sum -c test_tree.md5 || { echo "Test tree failed md5 after addition"; exit 1; }
@@ -34,7 +34,7 @@ md5sum -c test_tree_addition.md5 || { echo "Test tree addition failed md5 after 
 echo Test adding the same file again
 rm -r test_tree || { echo "Couldn't remove test tree"; exit 1; }
 tar -xvzf test_tree_addition.tar.gz || { echo "Couldn't create test tree for addition"; exit 1; }
-REPO_PATH=$REPO $CMD import -u test_tree MyTestSession || { echo "Couldn't import added files"; exit 1; }
+REPO_PATH=$REPO $CMD import test_tree MyTestSession || { echo "Couldn't import added files"; exit 1; }
 rm -r test_tree || { echo "Couldn't remove test tree"; exit 1; }
 REPO_PATH=$REPO $CMD co MyTestSession test_tree || { echo "Couldn't check out tree"; exit 1; }
 md5sum -c test_tree.md5 || { echo "Test tree failed md5 after addition"; exit 1; }
