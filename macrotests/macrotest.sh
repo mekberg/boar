8d52869b684c906fe1e5b978bfd8da4f83c65a04
Mats Ekberg <ekberg@gmail.com>
2010-10-03 13:04:26 +0000

    Import now reqiures --new-session to create a new session instead of doing it implicitly

--- macrotests/macrotest.sh
+++ macrotests/macrotest.sh
@@ -11,7 +11,7 @@ rm -r $REPO test_tree 2>/dev/null
 tar -xvzf test_tree.tar.gz || { echo "Couldn't create test tree"; exit 1; }
 md5sum -c test_tree.md5 || { echo "Test tree failed md5 before check-in"; exit 1; }
 $CMD mkrepo $REPO || { echo "Couldn't create repo"; exit 1; }
-REPO_PATH=$REPO $CMD import test_tree MyTestSession || { echo "Couldn't import tree"; exit 1; }
+REPO_PATH=$REPO $CMD import --new-session test_tree MyTestSession || { echo "Couldn't import tree"; exit 1; }
 rm -r test_tree || { echo "Couldn't remove test tree after import"; exit 1; }
 REPO_PATH=$REPO $CMD co MyTestSession test_tree || { echo "Couldn't check out tree"; exit 1; }
 md5sum -c test_tree.md5 || { echo "Test tree failed md5 after check-out"; exit 1; }
