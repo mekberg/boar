b7219ebf792227048a2747f6458096de7de1d9d7
Mats Ekberg <ekberg@gmail.com>
2010-10-31 17:16:30 +0000

    verifying sessions in cmd_verifyi

--- cmd.py
+++ cmd.py
@@ -6,6 +6,7 @@ from time import time
 import cProfile
 from optparse import OptionParser
 from blobrepo import repository
+from blobrepo.sessions import bloblist_fingerprint
 import client
 
 if sys.version_info >= (2, 6):
@@ -139,6 +140,18 @@ def cmd_list(front, args):
         print "Duuuh?"
 
 def cmd_verify(front, args):
+    print "Collecting a list of all sessions..."
+    session_ids = front.get_session_ids()
+    print "Verifying %s sessions" % (len(session_ids))
+    for i in range(0, len(session_ids)):
+        bloblist = front.get_session_bloblist(session_ids[i])
+        calc_fingerpring = bloblist_fingerprint(bloblist)
+        assert calc_fingerpring == front.get_session_property(session_ids[i], "fingerprint"), \
+            "Fingerprint didn't match for session "+str(session_ids[i])
+        for bi in bloblist:
+            assert front.has_blob(bi['md5sum']), "Session %s is missing blob %s" \
+                % (session_ids[i], bi['md5sum'])
+        print i, len(bloblist), "blobs ok"
     print "Collecting a list of all blobs..."
     count = front.init_verify_blobs()
     print "Verifying..."
