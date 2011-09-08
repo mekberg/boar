3e8259e6f3c4590556f014b3ecdf475ead924f55
Mats Ekberg <ekberg@gmail.com>
2011-09-08 21:38:56 +0200

    Added checksum for each row. Code cleanup.

--- blobrepo/derived.py
+++ blobrepo/derived.py
@@ -22,6 +22,7 @@ import sqlite3
 from weakref import proxy
 from common import *
 import atexit
+import repository
 
 class blobs_sha256:
     def __init__(self, repo, datadir):
@@ -34,61 +35,62 @@ class blobs_sha256:
         self.datadir = datadir
         self.conn = None
         self.__init_db()
-        atexit.register(self.__sync)
+        atexit.register(self.sync)
 
     def __init_db(self):
         if self.conn:
             return
         try:
             self.conn = sqlite3.connect(os.path.join(self.datadir, "sha256cache"), check_same_thread = False)
-            self.conn.execute("CREATE TABLE IF NOT EXISTS checksums (md5 char(32) PRIMARY KEY, sha256 char(64) NOT NULL)")
+            self.conn.execute("CREATE TABLE IF NOT EXISTS checksums (md5 char(32) PRIMARY KEY, sha256 char(64) NOT NULL, row_md5 char(32))")
             self.conn.commit()
-        except Exception, e:
-            warn("Exception while initializing blobs_sha256 derived database - harmless but things may be slow\n")
-            warn("The reason was: "+ str(e))
-            self.__reset()
+            print "Db at %s opened with no problems" % os.path.join(self.datadir, "sha256cache")
+        except sqlite3.DatabaseError, e:
+            raise repository.SoftCorruptionError(e)
+            #raise repository.SoftCorruptionError("Exception while accessing the sha256 cache: "+str(e))
 
     def __set_result(self, md5, sha256):
+        md5_row = md5sum(md5 + sha256)
         try:
-            self.conn.execute("INSERT INTO checksums (md5, sha256) VALUES (?, ?)", (md5, sha256))
-            # No commit here - too slow. Let's do it at exit instead
-        except Exception, e:
-            warn("Exception while writing to blobs_sha256 derived database - harmless but things may be slow\n")
-            warn("The reason was: "+ str(e))
-            self.__reset()
-
+            self.conn.execute("INSERT INTO checksums (md5, sha256, row_md5) VALUES (?, ?)", (md5, sha256, md5_row))
+        except sqlite3.DatabaseError, e:
+            raise repository.SoftCorruptionError("Exception while writing to the sha256 cache: "+str(e))
+        print "Result written to db"
 
     def __get_result(self, md5):
         try:
             c = self.conn.cursor()
             c.execute("SELECT sha256 FROM checksums WHERE md5 = ?", (md5,))
             rows = c.fetchall()
-            if rows:
-                assert len(rows) == 1
-                return rows[0][0]
-        except:
-            warn("Exception while reading from blobs_sha256 derived database - harmless but things may be slow\n")
-            self.__reset()
-        return None
+        except sqlite3.DatabaseError, e:
+            raise repository.SoftCorruptionError("Exception while reading from the sha256 cache: "+str(e))
+        if not rows:
+            print "Result not found in cache"
+            return None
+        assert len(rows) == 1
+        md5, sha256, md5_row = rows[0]
+        if md5_row != md5sum(md5 + sha256):
+            raise repository.SoftCorruptionError("Integrity check failed on a row in sha256 cache")
+        print "Result successfully loaded from cache"
+        return sha256
 
     def verify(self):
         try:
             c = self.conn.cursor()
             c.execute("SELECT md5, sha256 FROM checksums")
-            rows = c.fetchall()
-            print "Sha256 cache verifying %s items" % len(rows)
-            for row in rows:
-                md5, sha256 = row
-                fresh_sha256 = self.__generate_sha256(md5)
-                if fresh_sha256 != sha256:
-                    warn("Stored sha256 does not match calculated value")
-                    return False
-                notice("Stored sha256 for %s seems correct" % md5)
-
         except Exception, e:
             warn("Exception while verifying sha256 storage: "+str(e))
             self.__reset()
             return False
+        rows = c.fetchall()
+        print "Sha256 cache verifying %s items" % len(rows)
+        for row in rows:
+            md5, sha256 = row
+            fresh_sha256 = self.__generate_sha256(md5)
+            if fresh_sha256 != sha256:
+                warn("Stored sha256 does not match calculated value")
+                return False
+            notice("Stored sha256 for %s seems correct" % md5)
         return True
 
     def __reset(self):
@@ -120,6 +122,14 @@ class blobs_sha256:
         self.__set_result(blob, result)
         return result
         
-    def __sync(self):
+    def sync(self):
+        print "Syncing db to disk"
         if self.conn:
             self.conn.commit()
+
+    def close(self):
+        self.sync()
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+
