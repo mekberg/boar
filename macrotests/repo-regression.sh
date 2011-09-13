944f20a75db78de79c4e9a4e93bac058a87443b6
Mats Ekberg <ekberg@gmail.com>
2011-09-13 21:05:50 +0200

    Fixed tests. They now all pass.

--- macrotests/repo-regression.sh
+++ macrotests/repo-regression.sh
@@ -10,21 +10,32 @@ tar xzf $TESTHOME/regression-boar-daily.11-Jul-2011.tar.gz || exit 1
 REPO=$testdir/regression-boar-daily.11-Jul-2011/TESTREPO
 cd regression-boar-daily.11-Jul-2011/workdir
 
-# Relocate the workdir pointer to the repo.
-patch $testdir/regression-boar-daily.11-Jul-2011/workdir/.meta/info <<EOF || exit 1
-5c5
-<     "repo_path": "/tmp/TESTREPO"
----
->     "repo_path": "$REPO"
+# Relocate the workdir pointer to the repo.  Also, since the test data
+# contains a half-finished commit, let's change the session_id to
+# point at the latest revision (the one in progress of being
+# committed). This should probably be done with some future boar
+# commands eventually.
+cat >$testdir/regression-boar-daily.11-Jul-2011/workdir/.meta/info <<EOF || exit 1
+{
+    "session_name": "Test",
+    "offset": "",
+    "session_id": 7,
+    "repo_path": "$REPO"
+}
 EOF
 
-REPO_PATH=$REPO $BOAR status || { echo "Couldn't execute status"; exit 1; }
+REPO_PATH=$REPO $BOAR status >$testdir/status.txt || { echo "Couldn't execute status"; exit 1; }
+if [ `grep -c . $testdir/status.txt` -ne 1 ]; then 
+    echo "Did not expect status to show any changes"; exit 1; 
+fi
 md5sum -c ../r7.md5 || { echo "r7.md5 failed"; exit 1; }
 REPO_PATH=$REPO $BOAR update -r2 || { echo "Couldn't execute update"; exit 1; }
 md5sum -c ../r2.md5 || { echo "r2.md5 failed"; exit 1; }
-REPO_PATH=$REPO $BOAR update || { echo "Couldn't execute status"; exit 1; }
+REPO_PATH=$REPO $BOAR update -r6 || { echo "Couldn't execute status"; exit 1; }
 md5sum -c ../r6.md5 || { echo "r6.md5 failed"; exit 1; }
 cp some_text.txt some_text2.txt
+REPO_PATH=$REPO $BOAR ci && { echo "Ci executed ok even though workdir is out of date"; exit 1; }
+REPO_PATH=$REPO $BOAR update || { echo "Couldn't execute update to latest version"; exit 1; }
 REPO_PATH=$REPO $BOAR ci || { echo "Couldn't execute ci"; exit 1; }
 REPO_PATH=$REPO $BOAR verify || { echo "Couldn't verify"; exit 1; }
 
