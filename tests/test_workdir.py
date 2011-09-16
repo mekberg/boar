a6b037b8889e414aefbd176ee258f2161aff454f
Mats Ekberg <ekberg@gmail.com>
2011-09-16 19:14:24 +0200

    Added tests for ignore/include

--- tests/test_workdir.py
+++ tests/test_workdir.py
@@ -432,6 +432,34 @@ class TestWorkdir(unittest.TestCase, WorkdirHelper):
         co_tree = read_tree(wd.root, skiplist = boar_dirs)
         self.assertEquals({'file.txt': 'f1'}, co_tree)
 
+    def testInclude(self):
+        tree = {'file.txt': 'f1',
+                'file.ignore': 'f2'}
+        wd = self.createWorkdir(self.repoUrl, tree)
+        wd.front.set_session_include_list("TestSession", ["*.txt"])
+        wd.checkout()
+        wd.checkin()
+
+        wd = self.createWorkdir(self.repoUrl)
+        wd.checkout()
+        co_tree = read_tree(wd.root, skiplist = boar_dirs)
+        self.assertEquals({'file.txt': 'f1'}, co_tree)
+
+    def testIgnoreInclude(self):
+        tree = {'file.txt': 'f1',
+                'file.ignore': 'f2'}
+        wd = self.createWorkdir(self.repoUrl, tree)
+        wd.front.set_session_include_list("TestSession", ["file.*"])
+        wd.front.set_session_ignore_list("TestSession", ["*.ignore"])
+        wd.checkout()
+        wd.checkin()
+
+        wd = self.createWorkdir(self.repoUrl)
+        wd.checkout()
+        co_tree = read_tree(wd.root, skiplist = boar_dirs)
+        self.assertEquals({'file.txt': 'f1'}, co_tree)
+
+
     def testIgnoreStickyness(self):
         tree = {'file.txt': 'f1',
                 'file.ignore': 'f2'}
@@ -458,6 +486,26 @@ class TestWorkdir(unittest.TestCase, WorkdirHelper):
         wd.checkin()
 
         wd.front.set_session_ignore_list("TestSession", ["*.ignore"])
+        wd.update(log = DevNull())
+        write_tree(wd.root, {'file-modified.ignore': 'f3 mod'}, False)
+        wd.checkin()
+
+        wd = self.createWorkdir(self.repoUrl)
+        wd.checkout()
+        co_tree = read_tree(wd.root, skiplist = boar_dirs)
+        self.assertEquals(tree, co_tree)
+
+    def testIncludeModifications(self):
+        """Expected behavior is that modifications of previously
+        committed (but now ignored) files should be ignored. But they
+        should still be checked out if they exist."""
+        tree = {'file.txt': 'f1',
+                'file.ignore': 'f2',
+                'file-modified.ignore': 'f3'}
+        wd = self.createWorkdir(self.repoUrl, tree)
+        wd.checkin()
+
+        wd.front.set_session_include_list("TestSession", ["*.txt"])
         wd.update(log = DevNull())
         write_tree(wd.root, {'file-modified.ignore': 'f3 mod'}, False)
         wd.checkin()
