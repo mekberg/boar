18487fadb774c36fd7b2c3059e372a8b4513ebef
Mats Ekberg <ekberg@gmail.com>
2012-02-05 15:53:33 +0100

    Regression tests renamed to test_issueN.sh

--- macrotests/test_issue14.sh
+++ macrotests/test_issue14.sh
@@ -3,21 +3,18 @@
 # Test concurrent commits
 
 testname="issue14"
-boar="`pwd`/../boar"
-testdir="/tmp/${testname}_tmp$$"
-TESTREPO="${testdir}/${testname}_TESTREPO"
-mkdir $testdir || exit 1
+TESTREPO="`pwd`/TESTREPO"
 
-$boar mkrepo $TESTREPO || exit 1
-$boar --repo=$TESTREPO mksession "TestSession" || exit 1
-$boar --repo=$TESTREPO co TestSession $testdir/wd1 || exit 1
-$boar --repo=$TESTREPO co TestSession $testdir/wd2 || exit 1
+$BOAR mkrepo $TESTREPO || exit 1
+$BOAR --repo=$TESTREPO mksession "TestSession" || exit 1
+$BOAR --repo=$TESTREPO co TestSession wd1 || exit 1
+$BOAR --repo=$TESTREPO co TestSession wd2 || exit 1
 
-dd if=/dev/urandom of=$testdir/wd1/bigfile.txt bs=1024 count=1024 || exit 1
-echo "Some data" >$testdir/wd2/afile.txt
+dd if=/dev/urandom of=wd1/bigfile.txt bs=1024 count=1024 || exit 1
+echo "Some data" >wd2/afile.txt
 sync
-cd $testdir/wd1 
-$boar ci &
+cd wd1 
+$BOAR ci &
 pid=$!
 
 # We do not want to rely on just timing here... We'll let the first
@@ -45,10 +42,10 @@ fi
 
 echo "First commit is in progress and has been paused."
 echo "Performing second commit (should fail)"
-( cd $testdir/wd2 && $boar ci ) && { echo "*** Test failed. Concurrent checkin succeeded, should fail." ; exit 1; }
+( cd wd2 && $BOAR ci ) && { echo "*** Test failed. Concurrent checkin succeeded, should fail." ; exit 1; }
 echo "Second commit done"
 echo "Resuming first commit"
 kill -SIGCONT $pid
 wait $pid || { echo "*** Test failed. First commit failed. Should succeed." ; exit 1; }
 
-rm -r $testdir
+
---
+++ macrotests/test_issue21.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Test option to ignore errors during import
+
+# Test for simple unreadable file
+mkdir workdir || exit 1
+echo "file1" >workdir/file.txt || exit 1
+echo "file2" >workdir/file_unreadable.txt || exit 1
+chmod a-r workdir/file_unreadable.txt || exit 1
+$BOAR mkrepo TESTREPO || exit 1
+$BOAR --repo=TESTREPO mksession "Test" || exit 1
+$BOAR --repo=TESTREPO import workdir "Test/workdir" && { echo "Import of non-readable file succeeded - should fail"; exit 1; }
+$BOAR --repo=TESTREPO import --ignore-errors workdir "Test/workdir" || { echo "Import failed even though --ignore-errors was given"; exit 1; }
+chmod -R u+r workdir || exit 1
+rm -r workdir || exit 1
+rm -r TESTREPO || exit 1
+echo "Test part 1 succeeded"
+
+# Test for file that is readable during changes scan, but unreadable during actual checkin
+cat >make_unreadable_at_commit.py <<"EOF"
+def modify_fn(fn):
+    def newfn(*args, **kwargs):
+        import os
+        os.chmod("workdir/file_unreadable.txt", 0000)
+        try:
+            return fn(*args, **kwargs)
+        finally:
+            os.chmod("workdir/file_unreadable.txt", 0744)
+    return newfn
+
+workdir.check_in_file = modify_fn(workdir.check_in_file)
+EOF
+mkdir workdir || exit 1
+echo "file1" >workdir/file.txt || exit 1
+echo "file2" >workdir/file_unreadable.txt || exit 1
+$BOAR mkrepo TESTREPO || exit 1
+$BOAR --repo=TESTREPO mksession "Test" || exit 1
+$BOAR --EXEC make_unreadable_at_commit.py --repo=TESTREPO import workdir "Test/workdir" && { echo "Import of non-readable file succeeded - should fail"; exit 1; }
+$BOAR --EXEC make_unreadable_at_commit.py --repo=TESTREPO import --ignore-errors workdir "Test/workdir" || { echo "Import failed even though --ignore-errors was given"; exit 1; }
+chmod -R u+r workdir || exit 1
+echo "Test part 2 succeeded"
+
+### This section is disabled for now
+# # Repeat test for unreadable directory
+# mkdir $testdir || exit 1
+# cd $testdir || exit 1
+# mkdir workdir || exit 1
+# mkdir workdir/unreadable_dir || exit 1
+# echo "file1" >workdir/file.txt || exit 1
+# echo "file2" >workdir/unreadable_dir/file.txt || exit 1
+# chmod a-r workdir/unreadable_dir || exit 1
+# $BOAR mkrepo TESTREPO || exit 1
+# $BOAR --repo=TESTREPO mksession "Test" || exit 1
+# $BOAR --repo=TESTREPO import workdir "Test/workdir" && { echo "Import of non-readable directory succeeded - should fail"; exit 1; }
+# $BOAR --repo=TESTREPO import --ignore-errors workdir "Test/workdir" || { echo "Import of unreadable dir failed even though --ignore-errors was given"; exit 1; }
+# chmod -R u+r workdir || exit 1
+# rm -r $testdir || exit 1
+# echo "Test part 2 succeeded"
+
