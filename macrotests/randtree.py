57d9c4e3bd4ae9da18f6ad772128493248834a15
Mats Ekberg <ekberg@gmail.com>
2012-02-11 17:01:02 +0100

    Added functionality for creating a full random tree

--- macrotests/randtree.py
+++ macrotests/randtree.py
@@ -29,13 +29,17 @@ def get_random_filename(size = 5):
         result += random.choice(allowed_chars)
     return result
 
-def add_randomized_file(path):
+def find_unused_filename(path, prefix = ""):
     random_base = get_random_filename()
-    filename = path + "/" + random_base + ".bin"
+    filename = path + "/" + prefix + random_base + ".bin"
     index = 0
     while os.path.exists(filename):
         index += 1
         filename = path + "/" + random_base + str(index) + ".bin"
+    return filename
+
+def add_randomized_file(path):
+    filename = find_unused_filename(path)
     write_random_contents(filename, 1)
 
 def write_random_contents(filename, count, blocksize = 1024):
@@ -49,10 +53,24 @@ def add_random_files(path, count, filesize_kb):
     for x in range(0, count):
         add_randomized_file(path)    
 
+def add_random_tree(path, total_dirs, total_files):
+    existing_dirs = [path]
+    for n in xrange(total_dirs):
+        parent = random.choice(existing_dirs)
+        new_dir = find_unused_filename(parent, prefix = "dir_")
+        os.mkdir(new_dir)
+        existing_dirs.append(new_dir)
+    for n in xrange(total_files):
+        parent = random.choice(existing_dirs)
+        add_randomized_file(parent)
+
 def main():
     random.seed(0) # Make deterministic
-    dirname, count, size = sys.argv[1:]
-    add_random_files(dirname, int(count), int(size))
+    #dirname, count, size = sys.argv[1:]
+    #add_random_files(dirname, int(count), int(size))
+    dirname, dircount, filecount = sys.argv[1:]
+    os.mkdir(dirname)
+    add_random_tree(dirname, int(dircount), int(filecount))
 
 if __name__ == "__main__":
     main()
