16a50697d864e178479421eef4826d4527afcbd7
Mats Ekberg <ekberg@gmail.com>
2012-02-13 22:49:56 +0100

    Fixed erronous message about workdir version upgrade.

--- macrotests/test_import.sh
+++ macrotests/test_import.sh
@@ -1,3 +1,20 @@
 set -e
 
 $BOAR import | grep "Usage: boar import" || { echo "Import with no args should give usage information"; exit 1; }
+
+export REPO_PATH=IMPORT_TEST_REPO
+$BOAR mkrepo $REPO_PATH || exit 1
+$BOAR mksession TestSession || exit 1
+mkdir Import || exit 1
+echo "Some data" >Import/file.txt || exit 1
+
+$BOAR import -wq Import TestSession || exit 1
+
+( cd Import && $BOAR status -vq >../status.txt 2>&1 ) || exit 1
+head -n -1 status.txt >status_no_timing.txt
+diff status_no_timing.txt - <<EOF || { echo "Status gave unexpected output"; exit 1; }
+  file.txt
+EOF
+
+true
+
--- tests/test_workdir.py
+++ tests/test_workdir.py
@@ -542,7 +542,8 @@ class TestWorkdir(unittest.TestCase, WorkdirHelper):
         full_tree_filenames = set(read_tree(wd.root).keys())
         expected_filenames = set([u'file.txt', 
                                   u'.boar/info', 
-                                  u'.boar/bloblistcache2.bin'])
+                                  u'.boar/bloblistcache2.bin',
+                                  u'.boar/wd_version.txt'])
 
         self.assertEquals(expected_filenames, full_tree_filenames)
 
