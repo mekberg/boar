e621430fdef1b71b4603bcff08030239f11b3121
Mats Ekberg <ekberg@gmail.com>
2012-02-18 21:07:55 +0100

    Now benchmarking import and co

--- macrotests/performance.sh
+++ macrotests/performance.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
+#for i in {1..10}; do ./performance.sh >>performance_log.txt; done
+
+if test -z "$TMP"; then
+    echo "You must set \$TMP before running this script"
+    exit 1
+fi
 boar=~+/`dirname $0`/../boar
 macrotests=~+/`dirname $0`/
-testdir="/tmp/boarperf_tmp$$"
+testdir="$TMP/boarperf_tmp$$"
+test_tree=$1
 
+# randtree.py test_tree 10 1000 1000000000 || exit 1
 
-# Test for simple unreadable file
 mkdir $testdir || exit 1
 cd $testdir || exit 1
-mkdir workdir || exit 1
-$macrotests/randtree.py workdir 5000 1
-$boar mkrepo TESTREPO || exit 1
-$boar --repo=TESTREPO mksession "Test" || exit 1
+$boar mkrepo TESTREPO >/dev/null || exit 1
+$boar --repo=TESTREPO mksession "Test" >/dev/null || exit 1
 sync
-echo "Test tree created. Importing..."
-time $boar --repo=TESTREPO import -w workdir "Test/workdir" || { echo "Import of benchmark tree failed"; exit 1; }
-time (cd workdir; $boar --repo=TESTREPO status;) || { echo "Status command failed"; exit 1; }
-du -b workdir
+
+t1=`date +"%s"`
+$boar --repo=TESTREPO import $test_tree Test >/dev/null|| { echo "Import of benchmark tree failed"; exit 1; }
+sync
+t2=`date +"%s"`
+echo "Time for import:" $[ $t2 - $t1 ] "seconds"
+
+
+t1=`date +"%s"`
+$boar --repo=TESTREPO co Test >/dev/null|| { echo "Co of benchmark tree failed"; exit 1; }
+sync
+t2=`date +"%s"`
+echo "Time for co:" $[ $t2 - $t1 ] "seconds"
+
+
 rm -r $testdir || exit 1
