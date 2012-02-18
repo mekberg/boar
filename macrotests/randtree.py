7ffb8b441e82c75b35fe42681f50248682971bd6
Mats Ekberg <ekberg@gmail.com>
2012-02-18 21:07:24 +0100

    Now possible to set total tree file size

--- macrotests/randtree.py
+++ macrotests/randtree.py
@@ -20,37 +20,33 @@ import sys
 import os
 import time
 import random
-from mkrandfile import mkrandfile
+from mkrandfile import mkrandfile_fast
 allowed_chars = u" abcdefghijklmnpqrstuvwxyzåäöABCDEFGHIJKLMNPQRSTUVWXYZÅÄÖ_0123456789"
 
-def get_random_filename(size = 5):
+def get_random_filename():
     result = ""
-    for x in range(0, size):
+    for x in range(0, random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 21, 50, 200])):
         result += random.choice(allowed_chars)
     return result
 
-def find_unused_filename(path, prefix = ""):
+def find_unused_filename(path, prefix = "", suffix = ""):
     random_base = get_random_filename()
-    filename = path + "/" + prefix + random_base + ".bin"
+    filename = path + "/" + prefix + random_base + suffix
     index = 0
     while os.path.exists(filename):
         index += 1
-        filename = path + "/" + random_base + str(index) + ".bin"
+        filename = path + "/" + prefix + random_base + str(index) + suffix
     return filename
 
-def add_randomized_file(path):
+def add_randomized_file(path, file_size = 0):
     filename = find_unused_filename(path)
-    write_random_contents(filename, 1)
+    write_random_contents(filename, file_size)
 
-def write_random_contents(filename, count):
+def write_random_contents(filename, file_size_bytes):
     assert not os.path.exists(filename)
-    mkrandfile(filename, count)
+    mkrandfile_fast(filename, file_size_bytes/1024)
 
-def add_random_files(path, count, filesize_kb):
-    for x in range(0, count):
-        add_randomized_file(path)    
-
-def add_random_tree(path, total_dirs, total_files):
+def add_random_tree(path, total_dirs, total_files, total_size):
     existing_dirs = [path]
     for n in xrange(total_dirs):
         print "Creating dirs", n, "\r",
@@ -63,15 +59,14 @@ def add_random_tree(path, total_dirs, total_files):
         print "Creating files", n, "\r",
         sys.stdout.flush()
         parent = random.choice(existing_dirs)
-        add_randomized_file(parent)
+        add_randomized_file(parent, total_size / total_files)
 
 def main():
     #dirname, count, size = sys.argv[1:]
-    #add_random_files(dirname, int(count), int(size))
-    dirname, dircount, filecount = sys.argv[1:]
+    dirname, dircount, filecount, total_size = sys.argv[1:]
     os.mkdir(dirname)
     random.seed((dircount, filecount)) # Make deterministic
-    add_random_tree(dirname, int(dircount), int(filecount))
+    add_random_tree(dirname, int(dircount), int(filecount), int(total_size))
 
 if __name__ == "__main__":
     main()
