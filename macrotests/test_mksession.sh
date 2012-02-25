aaab8f2982ef744d242a4065e52abb63896ee2ff
Mats Ekberg <ekberg@gmail.com>
2012-02-25 15:15:02 +0100

    Using txtmatch.py instead of patch for some tests

--- macrotests/test_mksession.sh
+++ macrotests/test_mksession.sh
@@ -1,9 +1,14 @@
 export REPO_PATH="`pwd`/TESTREPO" 
 $BOAR mkrepo $REPO_PATH || exit 1
 
+cat >expected_duplicate_msg.txt <<EOF
+ERROR: There already exists a session named 'SimpleSession'
+!Finished in .* seconds
+EOF
+
 $BOAR mksession SimpleSession || { echo "mksession for simple name failed"; exit 1; }
 $BOAR mksession SimpleSession && { echo "mksession for existing name should fail"; exit 1; }
-$BOAR mksession SimpleSession | grep "ERROR: There already exists a session named 'SimpleSession'" || \
+$BOAR mksession SimpleSession | txtmatch.py expected_duplicate_msg.txt ||
     { echo "Unexpected error message for duplicate session"; exit 1; }
 $BOAR mksession "#Räk smörgås" || { echo "mksession for tricky name failed"; exit 1; }
 $BOAR mksession "#Räk smörgås" && { echo "mksession for existing tricky name should fail"; exit 1; }
@@ -16,11 +21,14 @@ $BOAR mksession "Sim\\ple" && { echo "mksession for names containing backslash s
 $BOAR mksession "__Simple" && { echo "mksession for names starting with double underscore should fail"; exit 1; }
 $BOAR mksession "_Sim_ple_" || { echo "most session names with underscores are allowed"; exit 1; }
 
-$BOAR ls | head -n -1 >ls_output.txt || exit 1
-diff - ls_output.txt <<EOF || { echo "unexpected ls output"; exit 1; }
+cat >expected_ls_output.txt <<EOF
 #Räk smörgås (1 revs)
 _Sim_ple_ (1 revs)
 SimpleSession (1 revs)
+!Finished in .* seconds
 EOF
+
+$BOAR ls >ls_output.txt || exit 1
+txtmatch.py expected_ls_output.txt ls_output.txt || { echo "unexpected ls output"; exit 1; }
 
 true
