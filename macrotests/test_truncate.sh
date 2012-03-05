ae41495f42f56f7dadef256b59c8b73adde8893d
Mats Ekberg <ekberg@gmail.com>
2012-03-05 17:56:14 +0100

    Added deletion in non-truncated session to test get_orphan_blobs()

--- macrotests/test_truncate.sh
+++ macrotests/test_truncate.sh
@@ -21,6 +21,10 @@ rm TestSession/r3.txt || exit 1
 echo "Rev 4" >TestSession/r4.txt || exit 1
 (cd TestSession && $BOAR ci -q) || exit 1
 
+echo "Yet another file" >AnotherTestSession/yet_another.txt || exit 1
+rm AnotherTestSession/another.txt || exit 1
+(cd AnotherTestSession && $BOAR ci -q) || exit 1
+
 cat >expected_disallowed_msg.txt <<EOF
 ERROR: This repository does not allow destructive changes.
 !Finished in .* seconds
@@ -36,16 +40,16 @@ touch TESTREPO_truncated/ENABLE_PERMANENT_ERASE || exit 1
 
 
 echo --- Verify that all snapshots exist before truncation
-for snapshot in 1 2 3 4 5 6; do
+for snapshot in 1 2 3 4 5 6 7; do
     # Make sure that all expected snapshots exist before truncation
     test -e "TESTREPO_truncated/sessions/$snapshot" || { echo "Snapshot $snapshot should exist"; exit 1; }
 done
-test ! -e "TESTREPO_truncated/sessions/7" || { echo "Snapshot should not exist yet"; exit 1; }
+test ! -e "TESTREPO_truncated/sessions/8" || { echo "Snapshot should not exist yet"; exit 1; }
 
 
 echo --- Perform truncate
 cat >expected_truncate_msg.txt <<EOF
-Session TestSession has been truncated to revision 7
+Session TestSession has been truncated to revision 8
 !Finished in .* seconds
 EOF
 
@@ -63,10 +67,11 @@ done
 test -e "TESTREPO_truncated/sessions/2" || { echo "Untruncated snapshot 2 should exist"; exit 1; }
 test -e "TESTREPO_truncated/sessions/5" || { echo "Untruncated snapshot 5 should exist"; exit 1; }
 test -e "TESTREPO_truncated/sessions/7" || { echo "Independent snapshot 7 should exist"; exit 1; }
-test ! -e "TESTREPO_truncated/sessions/8" || { echo "Snapshot 7 should not exist"; exit 1; }
+test -e "TESTREPO_truncated/sessions/8" || { echo "Independent snapshot 8 should exist"; exit 1; }
+test ! -e "TESTREPO_truncated/sessions/9" || { echo "Snapshot 9 should not exist"; exit 1; }
 
 cat >expected_list_msg.txt <<EOF
-!Revision id 7 .*, 1 files, \(standalone\) Log: Standalone snapshot
+!Revision id 8 .*, 1 files, \(standalone\) Log: Standalone snapshot
 !Finished in .* seconds
 EOF
 
@@ -77,6 +82,7 @@ txtmatch.py expected_list_msg.txt list_msg.txt
 cat >expected_list_msg2.txt <<EOF
 !Revision id 2 .*, 0 files, \(standalone\) Log: <not specified>
 !Revision id 5 .*, 1 files, \(delta\) Log: <not specified>
+!Revision id 7 .*, 1 files, \(delta\) Log: <not specified>
 !Finished in .* seconds
 EOF
 
