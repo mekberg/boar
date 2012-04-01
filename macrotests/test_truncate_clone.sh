081d88780becb0bc374e0592105878cf0a9e02be
Mats Ekberg <ekberg@gmail.com>
2012-04-01 16:52:45 +0200

    Cloning deletions now works

--- macrotests/test_truncate_clone.sh
+++ macrotests/test_truncate_clone.sh
@@ -52,8 +52,22 @@ cat >expected.txt <<EOF
 !Finished in .* seconds
 EOF
 
-$BOAR --repo=TESTREPO_truncated_clone list -d >output.txt 2>&1 || exit 1
+$BOAR --repo=TESTREPO_truncated_clone list -d >output.txt 2>&1 || { 
+    cat output.txt; echo "Listing truncated clone failed"; exit 1; }
 txtmatch.py expected.txt output.txt || { echo "Unexpected list result"; exit 1; }
 $BOAR --repo=TESTREPO_truncated_clone verify || { echo "Truncated clone repo failed verify"; exit 1; }
+
+echo "--- Test that deleted snapshots are deleted on the other side when cloning"
+
+$BOAR clone TESTREPO TESTREPO_clone || { echo "Simple cloning failed"; exit 1; }
+
+$BOAR clone TESTREPO_truncated TESTREPO_clone && { echo "Cloning with deletions to a repo without enabled deletions should fail"; exit 1; }
+touch TESTREPO_clone/ENABLE_PERMANENT_ERASE || exit 1
+$BOAR clone TESTREPO_truncated TESTREPO_clone || { echo "Cloning deletions failed"; exit 1; }
+
+$BOAR --repo=TESTREPO_clone list -d >output.txt 2>&1 || { 
+    cat output.txt; echo "Listing cloned repo with deletions failed"; exit 1; }
+txtmatch.py expected.txt output.txt || { echo "Unexpected list result for repo with cloned deletions"; exit 1; }
+
 
 exit 0
