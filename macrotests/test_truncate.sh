53d8f989cd8c3f6db7605b797f81529ab3afdc9c
Mats Ekberg <ekberg@gmail.com>
2012-04-18 21:57:06 +0200

    Nice error message when truncating write protected sessions

--- macrotests/test_truncate.sh
+++ macrotests/test_truncate.sh
@@ -50,6 +50,22 @@ done
 test ! -e "TESTREPO_truncated/sessions/8" || { echo "Snapshot should not exist yet"; exit 1; }
 
 
+echo --- Verify that dump data matches expected output
+cat >expected.txt <<EOF
+[1, null, "TestSession", "d41d8cd98f00b204e9800998ecf8427e", null, false]
+[2, null, "AnotherTestSession", "d41d8cd98f00b204e9800998ecf8427e", null, false]
+[3, 1, "TestSession", "d0af0eeeab6af76afa221f25e8a45cbd", null, false]
+[4, 3, "TestSession", "4e35e0568a64e6b3c9dbaf26961deb38", null, false]
+[5, 2, "AnotherTestSession", "0e997688909a2d27886dfdeaa627b560", null, false]
+[6, 4, "TestSession", "ed6b2754f96ba1e3c1cf10ab3e492b03", null, false]
+[7, 5, "AnotherTestSession", "31a44468d11cc4924b15c5d106410a63", null, false]
+!Finished in .* seconds
+EOF
+
+$BOAR --repo=TESTREPO_truncated list --dump >output.txt 2>&1 || { echo "List command failed"; exit 1; }
+txtmatch.py expected.txt output.txt || { echo "Unexpected dump output"; exit 1; }
+
+
 echo --- Perform truncate
 cat >expected_truncate_msg.txt <<EOF
 Session TestSession has been truncated to revision 8
@@ -83,9 +99,42 @@ cat >expected_list_msg2.txt <<EOF
 EOF
 
 $BOAR --repo=TESTREPO_truncated list AnotherTestSession >list_msg2.txt 2>&1 || exit 1
-txtmatch.py expected_list_msg2.txt list_msg2.txt
+txtmatch.py expected_list_msg2.txt list_msg2.txt || exit 1
+
+echo --- Verify that dump data matches expected output
+cat >expected.txt <<EOF
+[1, null, "__deleted", "d41d8cd98f00b204e9800998ecf8427e", null, true]
+[2, null, "AnotherTestSession", "d41d8cd98f00b204e9800998ecf8427e", null, false]
+[3, null, "__deleted", "d41d8cd98f00b204e9800998ecf8427e", null, true]
+[4, null, "__deleted", "d41d8cd98f00b204e9800998ecf8427e", null, true]
+[5, 2, "AnotherTestSession", "0e997688909a2d27886dfdeaa627b560", null, false]
+[6, null, "__deleted", "d41d8cd98f00b204e9800998ecf8427e", null, true]
+[7, 5, "AnotherTestSession", "31a44468d11cc4924b15c5d106410a63", null, false]
+[8, null, "TestSession", "ed6b2754f96ba1e3c1cf10ab3e492b03", "Standalone snapshot", false]
+!Finished in .* seconds
+EOF
+
+$BOAR --repo=TESTREPO_truncated list --dump >output.txt 2>&1 || { echo "List command failed"; exit 1; }
+txtmatch.py expected.txt output.txt || { echo "Unexpected dump output"; exit 1; }
 
 echo --- Verify truncated repo
 $BOAR --repo=TESTREPO_truncated verify || { echo "Truncated repo failed verify"; exit 1; }
+
+echo --- Verify nice error messages on write protected repo
+cp -an TESTREPO TESTREPO_protected || exit 1
+touch TESTREPO_protected/ENABLE_PERMANENT_ERASE || exit 1
+chmod -R a-w TESTREPO_protected/sessions || exit 1
+
+cat >expected.txt <<EOF
+ERROR: Snapshot 6 is write protected, cannot erase. Change your repository file permissions and try again.
+!Finished in .* seconds
+EOF
+
+$BOAR --repo=TESTREPO_protected truncate TestSession >output.txt 2>&1 && {
+    cat output.txt; echo "Truncation of protected repo should fail"; exit 1; }
+
+chmod -R a+w TESTREPO_protected || exit 1
+
+txtmatch.py expected.txt output.txt || { echo "Unexpected error message for write protected session"; exit 1; }
 
 exit 0 # All is well
