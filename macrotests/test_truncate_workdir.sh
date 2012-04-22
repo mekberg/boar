1449625833081974990c4d50be75f22a5c036142
Mats Ekberg <ekberg@gmail.com>
2012-04-22 21:36:12 +0200

    Nice messages when using workdirs belonging to deleted snapshots

--- macrotests/test_truncate_workdir.sh
+++ macrotests/test_truncate_workdir.sh
@@ -34,6 +34,7 @@ echo "modified" >repo_modified.txt || exit 1
 rm repo_deleted.txt || exit 1
 $BOAR ci || exit 1
 cd ..
+(cd TestSession && $BOAR info | grep "Revision: 2") || { echo "Unexpected revision"; exit 1; }
 
 $BOAR --repo=TESTREPO truncate TestSession || exit 1
 
@@ -41,7 +42,7 @@ cat >expected.txt <<EOF
 [1, null, "__deleted", "d41d8cd98f00b204e9800998ecf8427e", null, true]
 [2, null, "__deleted", "d41d8cd98f00b204e9800998ecf8427e", null, true]
 [3, null, "__deleted", "d41d8cd98f00b204e9800998ecf8427e", null, true]
-[4, null, "TestSession", "5802b36c54f7136ce0a140ca7b946783", "Standalone snapshot", false]
+[4, null, "TestSession", "5802b36c54f7136ce0a140ca7b946783", null, false]
 !Finished in .* seconds
 EOF
 $BOAR --repo=TESTREPO list --dump >output.txt 2>&1 || { echo "Dump failed"; exit 1; }
@@ -66,10 +67,18 @@ EOF
 (cd TestSession && $BOAR status -q 2>&1) >output.txt && { cat output.txt; echo "Status for deleted snapshot should fail"; exit 1; }
 txtmatch.py expected.txt output.txt || { echo "Output mismatch at line $LINENO"; exit 1; }
 
-
-echo "Checking contents"
-(cd TestSession && $BOAR update -q) || { echo "Update failed"; exit 1; }
-
+cat >expected_update_output.txt <<EOF
+Skipping update of modified file repo_modified.txt
+Updating: wd_deleted.txt
+Skipping update of modified file wd_modified.txt
+WARNING: Because the old workdir revision was deleted, the update command was
+         unable to detect changes accurately. Make sure the workdir does not
+         contain out of date data before you commit.
+Workdir now at revision 4
+!Finished in .* seconds
+EOF
+(cd TestSession && $BOAR update -q 2>&1) >update_result.txt || { echo "Update failed"; exit 1; }
+txtmatch.py expected_update_output.txt update_result.txt || { echo "Output mismatch at line $LINENO"; exit 1; }
 
 # All the files should still be as in the original workdir, except that
 # wd_deleted.txt should be restored.
@@ -93,7 +102,38 @@ M wd_modified.txt
 EOF
 (cd TestSession && $BOAR status -vq) >output.txt || { echo "Status failed"; exit 1; }
 txtmatch.py expected_workdir_status.txt output.txt || { echo "Unexpected workdir status after update"; exit 1; }
-
 (cd TestSession && $BOAR info | grep "Revision: 4") || { echo "Unexpected revision"; exit 1; }
+
+rm -r TestSession || exit 1 # To avoid mistakes in the rest of the code
+
+echo "--- Test up-to-date workdir"
+cat >workdir_uptodate_contents.txt <<EOF
+repo_modified.txt:modified
+unchanged.txt:original 1
+wd_deleted.txt:original 5
+wd_modified.txt:original 4
+EOF
+
+(cd TestSessionModify && grep . *.txt) >output.txt || exit 1
+txtmatch.py workdir_uptodate_contents.txt output.txt || { echo "Unexpected uptodate workdir initial contents"; exit 1; }
+
+(cd TestSessionModify && $BOAR info | grep "Revision: 3") || { echo "Unexpected revision"; exit 1; }
+
+cat >expected_uptodate_status.txt <<EOF
+ERROR: The current snapshot has been deleted in the repository.
+!Finished in .* seconds
+EOF
+(cd TestSessionModify && $BOAR status -q 2>&1) >output.txt && { cat output.txt; echo "Status for deleted snapshot should fail"; exit 1; }
+txtmatch.py expected.txt output.txt || { echo "Output mismatch at line $LINENO"; exit 1; }
+
+
+cat >expected_uptodate_update.txt <<EOF
+Workdir now at revision 4
+!Finished in .* seconds
+EOF
+(cd TestSessionModify && $BOAR update -q 2>&1 ) >output.txt || { echo "Status failed"; exit 1; }
+txtmatch.py expected_uptodate_update.txt output.txt || { echo "Unexpected uptodate workdir update output"; exit 1; }
+(cd TestSessionModify && $BOAR info | grep "Revision: 4") || { echo "Unexpected revision"; exit 1; }
+
 
 exit 0 # All is well
