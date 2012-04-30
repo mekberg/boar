bb420563f164fe102e47c00711ca854da42a1a08
Mats Ekberg <ekberg@gmail.com>
2012-04-30 16:52:05 +0200

    Added connect_cmd(), removed old socket connect()

--- client.py
+++ client.py
@@ -20,42 +20,26 @@ import base64
 import re
 import sys
 
-def connect(url):
-    m = re.match("boar://(.*?)(:[0-9]+)?/", url)
-    assert m, "Not a valid boar url"
-    address = m.group(1)    
-    port = 50000
-    if m.group(2):
-        port = int(m.group(2)[1:])
-    #print "Connecting to '%s' on port %s" % (address, port)
-    server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(), 
-                                 jsonrpc.TransportTcpIp(addr=(address, port), 
-                                                        timeout=60.0, limit=2**16))
-    return server.front
+import subprocess
 
-
-import subprocess, time
-
-def connect_ssh(url):
-    print "Connecting with command"
-    url_match = re.match("boar\+ssh://(.*)@([^/]+)(/.*)", url)
-    assert url_match, "Not a valid Boar URL:" + url
-    user, host, path = url_match.groups()
-    cmd = "ssh '%s'@'%s' python hg/boar-contrib/server.py '%s'" % (user, host, path)
-    print user, host, path
+def connect_cmd(cmd):
     p = subprocess.Popen(cmd, 
                          shell = True, 
                          stdout = subprocess.PIPE, 
                          stdin = subprocess.PIPE, 
                          stderr = open("output-server.txt", "w"))
-    if p.poll() != None:
-        print "*** Command failed: ", cmd
-        sys.exit(1)
-    else:
-        print "*** Command running"
     server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(), 
                                  jsonrpc.TransportStream(p.stdout, p.stdin))
     assert server.front.ping() == "pong"
     return server.front
+
+def connect_ssh(url):
+    url_match = re.match("boar\+ssh://(.*)@([^/]+)(/.*)", url)
+    assert url_match, "Not a valid Boar URL:" + url
+    user, host, path = url_match.groups()
+    cmd = "ssh '%s'@'%s' python hg/boar-contrib/server.py '%s'" % (user, host, path)
+    print user, host, path
+    return connect_cmd(cmd)
+
 
 
