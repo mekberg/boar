83aee373f8994f8ce457972c844d837217a2f46d
Mats Ekberg <ekberg@gmail.com>
2012-06-06 17:19:09 +0200

    Testing log command in excercise

--- macrotests/excercise_repo.sh
+++ macrotests/excercise_repo.sh
@@ -7,9 +7,14 @@ REPO_PATH=$REPO $BOAR verify || { echo $prefix "Verify failed"; exit 1; }
 REPO_PATH=$REPO $BOAR ls || { echo $prefix "Ls failed"; exit 1; }
 REPO_PATH=$REPO $BOAR list -m || { echo $prefix "List -m failed"; exit 1; }
 REPO_PATH=$REPO $BOAR list --dump || { echo $prefix "List --dump failed"; exit 1; }
+REPO_PATH=$REPO $BOAR log || { echo $prefix "Log failed"; exit 1; }
+REPO_PATH=$REPO $BOAR log -v || { echo $prefix "Log failed"; exit 1; }
 
 SESSIONS=$(REPO_PATH=$REPO $BOAR ls |grep -v Finished| sed -e 's/ (.*)$//g')
 for session in $SESSIONS; do
+    # Show the log for the session
+    REPO_PATH=$REPO $BOAR log -v $session || { echo $prefix "Log for session $session failed"; exit 1; }
+
     # Simple checkout of latest revision
     CO_NAME=`mktemp -u EXCERCISE_CO_XXXX`
     REPO_PATH=$REPO $BOAR co $session "$CO_NAME" || { echo $prefix "Co of session '$session' failed"; exit 1; }
@@ -18,6 +23,11 @@ for session in $SESSIONS; do
     # Check out every revision of this session explicitly
     REVISIONS=$($BOAR --repo=$REPO list $session |grep "Revision id"|cut -d ' ' -f 3)
     for revision in $REVISIONS; do
+        # Test ls for every revision
+        REPO_PATH=$REPO $BOAR ls -r $revision $session || { echo $prefix "Ls for session $session rev $revision failed"; exit 1; }
+        REPO_PATH=$REPO $BOAR ls -v -r $revision $session || { echo $prefix "Ls -v for session $session rev $revision failed"; exit 1; }
+	
+	# Test checking out every revision
 	CO_NAME=`mktemp -u EXCERCISE_CO_XXXX`
 	REPO_PATH=$REPO $BOAR co -r $revision $session "$CO_NAME" || { 
 	    echo $prefix "Co of session '$session' revision $revision failed"; exit 1; }
