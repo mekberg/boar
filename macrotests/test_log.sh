4641ff8b47b9115525a51f031f18e0f4f6a47982
Mats Ekberg <ekberg@gmail.com>
2012-06-11 22:47:39 +0200

    Added tests for log command repo spec priority

--- macrotests/test_log.sh
+++ macrotests/test_log.sh
@@ -128,4 +128,34 @@ $BOAR log --repo=TESTREPO TestSession -r5 >output.txt || exit 1
 txtmatch.py expected.txt output.txt || { 
     echo "Unexpected range 5 log output"; exit 1; }
 
+#
+# Testing different repo specfications priority (command
+# line, workdir, environment)
+#
+
+$BOAR mkrepo REPO_WORKDIR || exit 1
+$BOAR --repo=REPO_WORKDIR mksession WorkdirRepo || exit 1
+$BOAR mkrepo REPO_CMDLINE || exit 1
+$BOAR --repo=REPO_CMDLINE mksession CmdlineRepo || exit 1
+$BOAR mkrepo REPO_ENV || exit 1
+$BOAR --repo=REPO_ENV mksession EnvRepo || exit 1
+
+$BOAR --repo=REPO_WORKDIR co WorkdirRepo || { echo "Couldn't check out WorkdirRepo"; exit 1; }
+
+$BOAR log && { echo "log without repo should fail"; exit 1; }
+$BOAR log | grep "ERROR: You need to specify a repository to operate on" || { 
+    echo "log without repo gave unexpected error message"; exit 1; }
+( REPO_PATH=REPO_ENV $BOAR log | grep EnvRepo ) || { echo "EnvRepo log failed"; exit 1; }
+( $BOAR --repo=REPO_CMDLINE log | grep CmdlineRepo ) || { echo "CmdlineRepo log failed"; exit 1; }
+( cd WorkdirRepo; $BOAR log | grep WorkdirRepo ) || { echo "WorkdirRepo log failed"; exit 1; }
+
+( REPO_PATH=REPO_ENV $BOAR --repo=REPO_CMDLINE log | grep CmdlineRepo ) || { 
+    echo "Cmdline should have priority for cmdline+env"; exit 1; }
+( cd WorkdirRepo; $BOAR --repo=REPO_CMDLINE log | grep CmdlineRepo ) || { 
+    echo "Cmdline should have priority for cmdline+workdir"; exit 1; }
+( cd WorkdirRepo; REPO_PATH=REPO_ENV $BOAR log | grep WorkdirRepo ) || {
+    echo "WorkdirRepo should have priority for workdir+env"; exit 1; }
+( cd WorkdirRepo; REPO_PATH=REPO_ENV $BOAR --repo=REPO_CMDLINE log | grep CmdlineRepo ) || { 
+    echo "Cmdline should have priority for workdir+env+cmdline"; exit 1; }
+
 exit 0 # All is well
