bbd64d25049ad2dab8a75d5c6d3f44497e4c24d8
Mats Ekberg <ekberg@gmail.com>
2012-06-11 23:52:48 +0200

    Fixed error in log tests

--- macrotests/test_log.sh
+++ macrotests/test_log.sh
@@ -143,19 +143,22 @@ $BOAR --repo=REPO_ENV mksession EnvRepo || exit 1
 $BOAR --repo=REPO_WORKDIR co WorkdirRepo || { echo "Couldn't check out WorkdirRepo"; exit 1; }
 
 $BOAR log && { echo "log without repo should fail"; exit 1; }
-$BOAR log | grep "ERROR: You need to specify a repository to operate on" || { 
+$BOAR log | grep "ERROR: You must use this command in a workdir or specify a repository to operate on" || { 
     echo "log without repo gave unexpected error message"; exit 1; }
 ( REPO_PATH=REPO_ENV $BOAR log | grep EnvRepo ) || { echo "EnvRepo log failed"; exit 1; }
 ( $BOAR --repo=REPO_CMDLINE log | grep CmdlineRepo ) || { echo "CmdlineRepo log failed"; exit 1; }
 ( cd WorkdirRepo; $BOAR log | grep WorkdirRepo ) || { echo "WorkdirRepo log failed"; exit 1; }
 
-( REPO_PATH=REPO_ENV $BOAR --repo=REPO_CMDLINE log | grep CmdlineRepo ) || { 
+REPO_ENV=`pwd`/REPO_ENV
+REPO_CMDLINE=`pwd`/REPO_CMDLINE
+
+( REPO_PATH=REPO_ENV $BOAR --repo=$REPO_CMDLINE log | grep CmdlineRepo ) || { 
     echo "Cmdline should have priority for cmdline+env"; exit 1; }
-( cd WorkdirRepo; $BOAR --repo=REPO_CMDLINE log | grep CmdlineRepo ) || { 
+( cd WorkdirRepo; $BOAR --repo=$REPO_CMDLINE log | grep CmdlineRepo ) || { 
     echo "Cmdline should have priority for cmdline+workdir"; exit 1; }
-( cd WorkdirRepo; REPO_PATH=REPO_ENV $BOAR log | grep WorkdirRepo ) || {
+( cd WorkdirRepo; REPO_PATH=$REPO_ENV $BOAR log | grep WorkdirRepo ) || {
     echo "WorkdirRepo should have priority for workdir+env"; exit 1; }
-( cd WorkdirRepo; REPO_PATH=REPO_ENV $BOAR --repo=REPO_CMDLINE log | grep CmdlineRepo ) || { 
+( cd WorkdirRepo; REPO_PATH=$REPO_ENV $BOAR --repo=$REPO_CMDLINE log | grep CmdlineRepo ) || { 
     echo "Cmdline should have priority for workdir+env+cmdline"; exit 1; }
 
 exit 0 # All is well
