b9c3034bc878079e26e565a96f509a5013ab2a70
Mats Ekberg <ekberg@gmail.com>
2012-06-27 01:10:11 +0200

    Testing ssh connections as part of run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -7,5 +7,7 @@ done
 
 echo "Executing local macro tests"
 BOAR_TEST_REMOTE_REPO=0 macrotests/macrotest.sh || { echo "Macrotests (local) failed"; exit 1; }
-echo "Executing remote macro tests"
+echo "Executing simulated remote macro tests"
 BOAR_TEST_REMOTE_REPO=1 macrotests/macrotest.sh || { echo "Macrotests (remote) failed"; exit 1; }
+echo "Executing ssh remote macro tests"
+BOAR_TEST_REMOTE_REPO=2 macrotests/macrotest.sh || { echo "Macrotests (remote) failed"; exit 1; }
