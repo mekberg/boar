e118a4b9f0abf5147209949d2041547890c95e9e
Mats Ekberg <ekberg@gmail.com>
2012-07-11 23:10:42 +0200

    Testing cat of multiple blobs

--- macrotests/test_cat.sh
+++ macrotests/test_cat.sh
@@ -6,12 +6,12 @@ $BOAR mksession --repo=$REPO_PATH RäksmörgåsSession || exit 1
 $BOAR co RäksmörgåsSession || exit 1
 
 echo "Rev 2" >RäksmörgåsSession/r2.txt || exit 1
-echo "Rev 2" >RäksmörgåsSession/modified.txt || exit 1
+echo "Modified rev 2" >RäksmörgåsSession/modified.txt || exit 1
 (cd RäksmörgåsSession && $BOAR ci -q) || exit 1
 
 rm RäksmörgåsSession/r2.txt || exit 1
 echo "Rev 3" >RäksmörgåsSession/r3.txt || exit 1
-echo "Rev 3" >RäksmörgåsSession/modified.txt || exit 1
+echo "Modified rev 3" >RäksmörgåsSession/modified.txt || exit 1
 cp /bin/ls RäksmörgåsSession/räksmörgås.bin || exit 1
 (cd RäksmörgåsSession && md5sum räksmörgås.bin >manifest-md5.txt) || exit 1
 (cd RäksmörgåsSession && $BOAR ci -q) || exit 1
@@ -21,6 +21,16 @@ $BOAR cat RäksmörgåsSession/r2.txt && { echo "Missing file should fail"; exit
 $BOAR cat -r2 RäksmörgåsSession/r2.txt | grep "Rev 2" || { echo "Unexpected output for cat -r2 r2.txt"; exit 1; }
 $BOAR cat RäksmörgåsSession/räksmörgås.bin >räksmörgås.bin || { echo "Binary file cat failed"; exit 1; }
 md5sum -c RäksmörgåsSession/manifest-md5.txt || exit 1
+
+cat >expected.txt <<EOF
+Rev 3
+Modified rev 3
+EOF
+
+$BOAR cat RäksmörgåsSession/r3.txt RäksmörgåsSession/modified.txt >output.txt || { 
+    cat output.txt; echo "Couldn't cat multiple files"; exit 1; }
+txtmatch.py expected.txt output.txt || { echo "Cat multiple files gave unexpected output"; exit 1; }
+
 
 echo "All is well"
 true
