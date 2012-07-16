abc55bd056ac6cf0fae0392a1773554ee5d2f224
Mats Ekberg <ekberg@gmail.com>
2012-07-16 13:14:23 +0200

    Test for cat command.

--- tests/test_cli_windows_specific.py
+++ tests/test_cli_windows_specific.py
@@ -19,10 +19,15 @@ from __future__ import with_statement
 import sys, os, unittest, tempfile, shutil
 import subprocess
 
-def call(cmd):
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+def call(cmd, check=True, cwd=None):
+    """Execute a command and return output and status code. If 'check' is True,
+    an exception will be raised if the command exists with an error code. If
+    'cwd' is set, the command will execute in that directory."""
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=cwd)
     stdout = p.communicate()[0]
     returncode = p.poll()
+    if check and returncode != 0:
+        raise Exception("Call failed with errorcode %s: %s" % (returncode, stdout))  
     return stdout, returncode
 
 BOAR_HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -33,6 +38,10 @@ if os.name == "nt":
     BOAR = os.path.join(BOAR_HOME, "boar.bat")
 else:
     BOAR = os.path.join(BOAR_HOME, "boar")
+
+def write_file(path, contents):
+    with open(path, "wb") as f:
+        f.write(contents)
 
 class TestCli(unittest.TestCase):
     def setUp(self):
@@ -45,12 +54,20 @@ class TestCli(unittest.TestCase):
 
     def testMkrepo(self):
         output, returncode = call([BOAR, "mkrepo", "TESTREPOåäö"])
-        assert returncode == 0
         assert os.path.exists("TESTREPOåäö")
-        output, returncode = call([BOAR, "mkrepo", "TESTREPOåäö"])
+        output, returncode = call([BOAR, "mkrepo", "TESTREPOåäö"], check=False)
         assert returncode == 1
         assert "ERROR: File or directory already exists" in output
 
+    def testCat(self):
+        testdata = "a\n\b\n\c"
+        call([BOAR, "mkrepo", "TESTREPOåäö"])
+        call([BOAR, "--repo", "TESTREPOåäö", "mksession", "TestSessionåäö"])
+        call([BOAR, "--repo", "TESTREPOåäö", "co", "TestSessionåäö"])
+        write_file("TestSessionåäö/fil.txt", testdata)
+        call([BOAR, "--repo", "TESTREPOåäö", "ci"], cwd="TestSessionåäö")
+        output, returncode = call([BOAR, "--repo", "TESTREPOåäö", "cat", "TestSessionåäö/fil.txt"])
+        self.assertEqual(output, testdata)
 
 class TestCliWindowsSpecific(unittest.TestCase):
     def setUp(self):
@@ -66,14 +83,13 @@ class TestCliWindowsSpecific(unittest.TestCase):
     #
 
     def testNoArgs(self):
-        output, returncode = call([BOAR])
+        output, returncode = call([BOAR], check=False)
         assert "Usage: boar" in output
         assert returncode == 1
 
     def testMkrepo(self):
         assert not os.path.exists("TESTREPO")
-        output, returncode = call([BOAR, "mkrepo", "TESTREPO"])
-        assert returncode == 0
+        call([BOAR, "mkrepo", "TESTREPO"])
         assert os.path.exists("TESTREPO")
 
     def testEmpty(self):
