7894597bd65c723d5f5829b008db40f028a86e28
Mats Ekberg <ekberg@gmail.com>
2013-01-31 22:03:00 +0100

    Now builds the rollingcs.so module correctly

--- cdedup/setup-cython.py
+++ cdedup/setup-cython.py
@@ -2,10 +2,12 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("rollingcs", ["rollingcs.pyx"])]
+ext_modules = [Extension("rollingcs", 
+                         ["rollingcs.pyx", "rollsum.c"]
+                         )]
 
 setup(
-  name = 'Rolling checksum',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
-)
+    name = 'Rolling checksum',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules
+    )
