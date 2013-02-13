cb7e5aa39cc7f5b98b4801a30bdd5a7f94fb71fd
Mats Ekberg <ekberg@gmail.com>
2013-02-13 22:00:52 +0100

    Added bitfield

---
+++ cdedup/bitfield.c
@@ -0,0 +1,71 @@
+#include "bitfield.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static inline void massert(const int condition, const char* const message) {
+  if(condition == 0){
+    printf("Assertion failed: %s\n", message);
+    exit(1);
+  }
+}
+
+#define BITS_PER_WORD (sizeof(unsigned int) * 8)
+
+BitField* bf_create(const unsigned int number_of_bits) {
+  BitField* bf = (BitField*) calloc(1, sizeof(BitField));
+  if(!bf) return NULL;
+  bf->size = number_of_bits;
+  bf->words = (unsigned int*) calloc(1 + (number_of_bits / BITS_PER_WORD), sizeof(unsigned int));
+  if(!bf->words) return NULL;
+  bf->sentinel = 0xDEADBEEF;
+  return bf;
+}
+
+inline void bf_set(BitField* const bf, const unsigned int bit, const int value){ 
+  massert(bit < bf->size, "Tried to write bit outside of range");
+  const unsigned int word = bit / BITS_PER_WORD;
+  const unsigned wbit = bit % BITS_PER_WORD;
+  bf->words[word] |= ((!!value) << wbit);
+}
+
+inline unsigned int bf_get(BitField* const bf, const unsigned int bit){
+  massert(bit < bf->size, "Tried to read bit outside of range");
+  const unsigned int word = bit / BITS_PER_WORD;
+  const unsigned wbit = bit % BITS_PER_WORD;
+  return !!(bf->words[word] & (1 << wbit));
+}
+
+void bf_print(BitField* bf) {
+  for(int c=0; c < bf->size; c++){
+    printf("%u", bf_get(bf, c));
+    if((c % 8) == 7) 
+      printf(" ");
+  }
+  printf("\n");
+}
+
+void bf_destroy(BitField* bf){
+  massert(bf != NULL, "Tried to destroy a null bitfield pointer");
+  massert(bf->sentinel == 0xDEADBEEF, "Bitfield sentinel failed");
+  free(bf->words);
+  free(bf);
+}
+
+int main() {
+  /* BitField* bf = bf_create(100); */
+  /* bf_set(bf, 99, 1); */
+  /* bf_print(bf); */
+  /* bf_destroy(bf); */
+  BitField* bf = bf_create(1000000);
+  massert(bf != NULL, "Mem alloc failed");
+  for(int n=0; n<100; n++){
+    for(int i=0; i<1000000; i++){
+      bf_set(bf, i, 1);
+    }
+    for(int i=0; i<1000000; i++){
+      bf_get(bf, i);
+    }
+  }
+  bf_destroy(bf);
+}
---
+++ cdedup/bitfield.h
@@ -0,0 +1,18 @@
+#ifndef BITFIELD_H
+#define BITFIELD_H
+
+typedef struct _Bitfield {
+  unsigned int size;
+  unsigned int* words;
+  unsigned int sentinel;
+} BitField;
+
+BitField* bf_create(const unsigned int number_of_bits);
+void bf_set(BitField* bf, unsigned int bit, int value);
+unsigned int bf_get(BitField* bf, unsigned int bit);
+void bf_print(BitField* bf);
+void bf_destroy(BitField* bf);
+
+int main();
+
+#endif
