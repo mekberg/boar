5aba100e901cf682c8a20725c2dda3d4a4468c66
Mats Ekberg <ekberg@gmail.com>
2013-02-24 23:23:07 +0100

    More tests

--- tests/test_deduplication.py
+++ tests/test_deduplication.py
@@ -30,7 +30,7 @@ from front import Front
 from wdtools import read_tree, write_tree, WorkdirHelper, boar_dirs
 from deduplication import print_recipe
 
-class TestWorkdir(unittest.TestCase, WorkdirHelper):
+class TestDeduplication(unittest.TestCase, WorkdirHelper):
     def setUp(self):
         self.remove_at_teardown = []
         self.workdir = self.createTmpName()
@@ -52,11 +52,11 @@ class TestWorkdir(unittest.TestCase, WorkdirHelper):
         recipe = self.repo.get_recipe(blob)
         self.assertEquals(len(recipe['pieces']), 2)
         self.assertEquals(recipe['pieces'][0], {
-                u'source': u'02129bb861061d1a052c592e2dc6b383', 
-                u'repeat': 1, u'original': True, u'offset': 0, u'size': 1})
+                'source': '02129bb861061d1a052c592e2dc6b383', 
+                'repeat': 1, 'original': True, 'offset': 0, 'size': 1})
         self.assertEquals(recipe['pieces'][1], {
-                u'source': u'00312b74e44d0712882387b8e0f0a57e', 
-                u'repeat': 1, u'original': False, u'offset': 0, u'size': 27})
+                'source': '00312b74e44d0712882387b8e0f0a57e', 
+                'repeat': 1, 'original': False, 'offset': 0, 'size': 27})
         rebuilt_content = self.wd.front.get_blob(blob).read()
         self.assertEquals(md5sum(rebuilt_content), "407badd3ba116d47c556d1366343048c")
 
@@ -66,13 +66,55 @@ class TestWorkdir(unittest.TestCase, WorkdirHelper):
         blob = self.addWorkdirFile("b.txt", "aaabbbccc")
         self.wd.checkin()
         recipe = self.repo.get_recipe(blob)
-        print_recipe(recipe)
+        #print_recipe(recipe)
         self.assertEquals(len(recipe['pieces']), 1)
         self.assertEquals(recipe['pieces'][0], {
-                u'source': first_blob, 
-                u'repeat': 1, u'original': False, u'offset': 18, u'size': 9})
+                'source': first_blob, 
+                'repeat': 1, 'original': False, 'offset': 18, 'size': 9})
         rebuilt_content = self.wd.front.get_blob(blob).read()
         self.assertEquals(md5sum(rebuilt_content), "d1aaf4767a3c10a473407a4e47b02da6")
+
+    def testSplitMatch(self):
+        a_blob = self.addWorkdirFile("a.txt", "aaa")
+        b_blob = self.addWorkdirFile("b.txt", "bbb")
+        self.wd.checkin()
+        c_blob = self.addWorkdirFile("c.txt", "aaabbb")
+        self.wd.checkin()
+        recipe = self.repo.get_recipe(c_blob)
+        #print_recipe(recipe)
+        self.assertEquals(len(recipe['pieces']), 2)
+        self.assertEquals(recipe['pieces'][1], {
+                'source': b_blob, 
+                'repeat': 1, 'original': False, 'offset': 0, 'size': 3})
+        rebuilt_content = self.wd.front.get_blob(c_blob).read()
+        self.assertEquals(md5sum(rebuilt_content), "6547436690a26a399603a7096e876a2d")
+
+    def testInterleavedHit(self):
+        a_blob = self.addWorkdirFile("a.txt", "aaa")
+        self.wd.checkin()
+        b_blob = self.addWorkdirFile("b.txt", "XaaaXaaaX")
+        self.wd.checkin()
+        x_blob = md5sum("X")
+        recipe = self.repo.get_recipe(b_blob)
+        #print_recipe(recipe)
+        self.assertEquals(len(recipe['pieces']), 5)
+        self.assertEquals(recipe['pieces'][0], {
+                'source': x_blob, 
+                'repeat': 1, 'original': True, 'offset': 0, 'size': 1})
+        self.assertEquals(recipe['pieces'][1], {
+                'source': a_blob, 
+                'repeat': 1, 'original': False, 'offset': 0, 'size': 3})
+        self.assertEquals(recipe['pieces'][2], {
+                'source': x_blob, 
+                'repeat': 1, 'original': True, 'offset': 0, 'size': 1})
+        self.assertEquals(recipe['pieces'][3], {
+                'source': a_blob, 
+                'repeat': 1, 'original': False, 'offset': 0, 'size': 3})
+        self.assertEquals(recipe['pieces'][4], {
+                'source': x_blob, 
+                'repeat': 1, 'original': True, 'offset': 0, 'size': 1})
+        rebuilt_content = self.wd.front.get_blob(b_blob).read()
+        self.assertEquals(md5sum(rebuilt_content), "e18585992d1ea79a30a34e015c49719e")
         
     def tearDown(self):
         for d in self.remove_at_teardown:
