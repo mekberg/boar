fe04064334663c4d05191875d79757e41cdd27b3
Mats Ekberg <ekberg@gmail.com>
2013-03-03 13:23:37 +0100

    Added circular buffer to build

--- cdedup/setup-cython.py
+++ cdedup/setup-cython.py
@@ -3,7 +3,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_modules = [Extension("rollingcs", 
-                         ["rollingcs.pyx", "rollsum.c", "intset.c", "bitfield.c"],
+                         ["rollingcs.pyx", "rollsum.c", "intset.c", "bitfield.c", "circularbuffer.c"],
                          extra_compile_args=["-O9", "-std=c99"],
                          )]
 
