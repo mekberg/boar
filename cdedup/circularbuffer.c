5c51e25ddb01fc668110cbd7c3515529a4e13293
Mats Ekberg <ekberg@gmail.com>
2013-03-03 12:28:44 +0100

    Added rotate_circular_buffer()

--- cdedup/circularbuffer.c
+++ cdedup/circularbuffer.c
@@ -16,7 +16,7 @@ CircularBuffer* create_circular_buffer(const uint32_t window_size){
   CircularBuffer* state = (CircularBuffer*) calloc(1, sizeof(CircularBuffer));
   assert(state != NULL, "CircularBuffer: Failed to allocate memory for state");
   state->circular_buffer_size = window_size;
-  state->physical_buffer_size = state->circular_buffer_size * 2;
+  state->physical_buffer_size = state->circular_buffer_size * 4;
   state->buf = calloc(1, state->physical_buffer_size);
   assert(state->buf != NULL, "CircularBuffer: Failed to allocate memory for buf");
   state->pos = 0;
@@ -56,6 +56,16 @@ char get_circular_buffer(CircularBuffer* state, const uint32_t pos){
   return state->buf[state->pos + pos];
 }
 
+char rotate_circular_buffer(CircularBuffer* state, const char c){
+  assert(is_full_circular_buffer(state), "CircularBuffer: Tried to rotate a non-full circular buffer");
+  char pushed_out = state->buf[state->pos];
+  push_circular_buffer(state, c);
+  return pushed_out;
+}
+
+int is_full_circular_buffer(CircularBuffer* state){
+  return state->length == state->circular_buffer_size;
+}
 
 void print_circular_buffer(CircularBuffer* state){
   printf("Buffer contents (%u bytes): '", state->length);
@@ -66,14 +76,15 @@ void print_circular_buffer(CircularBuffer* state){
 }
 
 /*
+
 int main() {
   CircularBuffer* state = create_circular_buffer(3);
-  for(int i=0;i<100000000; i++){
-    push_circular_buffer(state, 'j');
-  }
   push_circular_buffer(state, 'a');
   push_circular_buffer(state, 'b');
   push_circular_buffer(state, 'c');
+  for(int i=0;i<100000000; i++){
+    rotate_circular_buffer(state, 'j');
+  }
   push_circular_buffer(state, 'd');
   push_circular_buffer(state, 'e');
   push_circular_buffer(state, 'f');
@@ -82,4 +93,5 @@ int main() {
   push_circular_buffer(state, 'i');
   push_circular_buffer(state, 'j');
 }
+
 */
