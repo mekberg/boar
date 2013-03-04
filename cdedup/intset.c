e8fe41ef1a848a0a6e3e07dee0a427f9fe3ec7e8
Mats Ekberg <ekberg@gmail.com>
2013-03-04 22:03:15 +0100

    Optimized contains_intset()

--- cdedup/intset.c
+++ cdedup/intset.c
@@ -3,6 +3,7 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
+#include "stdint.h"
 
 static void massert(int condition, const char* message) {
   if(condition == 0){
@@ -11,8 +12,21 @@ static void massert(int condition, const char* message) {
   }
 }
 
+static int is_power_of_2(const uint32_t n){
+  uint32_t c = 1;
+  for(unsigned i=0; i<32; i++){
+    if(c == n)
+      return 1;
+    c <<= 1;
+  }
+  return 0;
+}
+
+#define qmod(a, b) ((a) & ((b)-1))
+
 IntSet* create_intset(const uint32_t bucket_count) {
   massert(bucket_count > 0, "Bucket count must not be zero");
+  massert(is_power_of_2(bucket_count), "Bucket count must be a power of 2");
   IntSet* intset = (IntSet*) calloc(1, sizeof(IntSet));
   intset->bucket_count = bucket_count;
   intset->buckets = (Bucket*) calloc(intset->bucket_count, sizeof(Bucket));
@@ -21,7 +35,7 @@ IntSet* create_intset(const uint32_t bucket_count) {
 }
 
 void add_intset(IntSet* intset, uint64_t int_to_add) {
-  Bucket* const bucket = &intset->buckets[int_to_add % intset->bucket_count];
+  Bucket* const bucket = &intset->buckets[qmod(int_to_add, intset->bucket_count)];
   //bf_set(intset->filter, int_to_add % intset->filter->size, 1);
   if(bucket->slot_count == 0) {
     bucket->slot_count = 1;
@@ -39,13 +53,27 @@ inline int contains_intset(IntSet* const intset, const uint64_t int_to_find) {
   //if(bf_get(intset->filter, int_to_find % intset->filter->size) == 0){
   //  return 0;
   // }
-  Bucket* const bucket = &intset->buckets[int_to_find % intset->bucket_count];
-  for(unsigned i=0; i < bucket->used_slots; i++){
-    if(bucket->slots[i] == int_to_find){
-      return 1;
+  Bucket* const bucket = &intset->buckets[qmod(int_to_find, intset->bucket_count)];
+  switch(bucket->used_slots) {
+    // Lets optimize this sucker...
+  case 0:
+    return 0;
+  case 1:
+    return bucket->slots[0] == int_to_find;
+  case 2:
+    return bucket->slots[0] == int_to_find || bucket->slots[1] == int_to_find;
+  case 4:
+    return bucket->slots[0] == int_to_find || bucket->slots[1] == int_to_find ||
+      bucket->slots[2] == int_to_find || bucket->slots[3] == int_to_find;
+  default:
+    for(unsigned i=0; i < bucket->used_slots; i++){
+      if(bucket->slots[i] == int_to_find){
+	return 1;
+      }
     }
+    return 0;
   }
-  return 0;
+  massert(0, "contains_intset(): Should not get here");
 }
 
 void destroy_intset(IntSet* intset) {
