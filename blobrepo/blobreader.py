533a829db3fb3af622598dda5b0e334a46a997a6
Mats Ekberg <ekberg@gmail.com>
2013-03-05 20:59:01 +0100

    Some speed improvements

--- blobrepo/blobreader.py
+++ blobrepo/blobreader.py
@@ -33,12 +33,22 @@ class RecipeReader(DataSource):
         self.local_path = local_path
 
         self.pieces = []
+        self.blob_paths = {} # Blob id -> blob path
 
         # Expand repeated pieces
         for piece in recipe['pieces']:
             repeat = piece.get('repeat', 1)
             for n in xrange(0, repeat):
                 self.pieces.append(piece)
+
+            blob = piece['source']
+            blobpath = None
+            if self.local_path:
+                blobpath =  os.path.join(self.local_path, blob)
+            if not blobpath or not os.path.exists(blobpath):
+                blobpath = self.repo.get_blob_path(blob)
+            assert os.path.exists(blobpath)
+            self.blob_paths[piece['source']] = blobpath
 
         self.blob_size = recipe['size']
         if size == None:
@@ -55,6 +65,7 @@ class RecipeReader(DataSource):
         self.__seek(self.pos)
         #print "Reader opening recipe:"
         #deduplication.print_recipe(recipe)
+        self.file_handles = {} # blob -> handle
 
     def remaining(self): # TODO: make less silly
         return self.bytes_left(self)
@@ -84,10 +95,21 @@ class RecipeReader(DataSource):
         assert self.source
         assert is_md5sum(self.source)
 
+
     def __readable_bytes_without_seek(self):
         pos_from_source_start = self.pos - self.blob_source_range_start
         readable = self.source_size - pos_from_source_start
         return readable
+
+    def __get_handle(self, path):
+        if path not in self.file_handles:
+            self.file_handles[path] = open(path, "rb")
+        return self.file_handles[path]
+
+    def __del__(self):
+        for f in self.file_handles.values():
+            f.close()
+        del self.file_handles
 
     def read(self, readsize = None):
         if readsize == None:
@@ -97,21 +119,19 @@ class RecipeReader(DataSource):
         readsize = min(self._bytes_left, readsize)
         result = ""
         while len(result) < readsize:
-            blobpath = None
-            if self.local_path:
-                blobpath = os.path.join(self.local_path, self.source)
-            if not blobpath or not os.path.exists(blobpath):
-                blobpath = self.repo.get_blob_path(self.source)
             bytes_left = readsize - len(result)
             bytes_to_read = min(self.__readable_bytes_without_seek(), bytes_left)
-            with open(blobpath, "rb") as f:
-                #print self.pos, self.blob_source_range_start, self.source_offset
-                source_file_pos = self.pos - self.blob_source_range_start + self.source_offset
-                source_file_size = os.path.getsize(blobpath)
-                assert source_file_pos <= source_file_size, "Source file %s is of unexpected size (seek to %s, is only %s)" % (blobpath, source_file_pos, source_file_size)
-                f.seek(source_file_pos)
-                bytes = f.read(bytes_to_read)
-                #print "Reader is reading from %s %s+%s" % (self.source, source_file_pos, bytes_to_read)
+
+            #print self.pos, self.blob_source_range_start, self.source_offset
+            blobpath = self.blob_paths[self.source]
+            source_file_pos = self.pos - self.blob_source_range_start + self.source_offset
+            source_file_size = os.path.getsize(blobpath)
+            assert source_file_pos <= source_file_size, "Source file %s is of unexpected size (seek to %s, is only %s)" % (blobpath, source_file_pos, source_file_size)
+            f = self.__get_handle(blobpath)
+            f.seek(source_file_pos)
+            bytes = f.read(bytes_to_read)
+            #print "Reader is reading from %s %s+%s" % (self.source, source_file_pos, bytes_to_read)
+
             assert len(bytes) == bytes_to_read
             result += bytes
             self.__seek(self.pos + len(bytes))
