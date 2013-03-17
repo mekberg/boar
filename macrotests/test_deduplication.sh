e894cc7dd40e3889063017baf9ab9be3a3744398
Mats Ekberg <ekberg@gmail.com>
2013-03-17 17:09:44 +0100

    More tests

--- macrotests/test_deduplication.sh
+++ macrotests/test_deduplication.sh
@@ -24,10 +24,15 @@ cp $BIGFILE Session/a.bin || exit 1
 
 (cd Session && 
     echo "Tjosan" >>a.bin &&
+    md5sum a.bin >>manifest.md5 &&
     $BOAR ci -q ) || exit 1
 
 test -d "$REPO/recipes" || exit 1
 $BOAR --repo="$REPO" verify || { echo "Verify failed"; exit 1; }
+
+rm -r Session || exit 1
+$BOAR --repo="$REPO" co Session || exit 1
+(cd Session && md5sum -c manifest.md5) || exit 1
 
 $BOAR clone "$REPO" "$CLONE" || exit 1
 test -d "$CLONE/recipes" || exit 1
@@ -53,3 +58,65 @@ test ! -z $(ls -A "$REPO/recipes") || { echo "Recipe dir should NOT be empty for
 test ! -z $(ls -A "$CLONE/recipes") || { echo "Cloned recipe dir should NOT be empty for dedup repo"; exit 1; }
 rm -r Session "$REPO" "$CLONE" || exit 1
 
+#
+# Test that a dedup repo cannot be created if the dedup module is
+# unavailable
+#
+
+BOAR_DISABLE_DEDUP=1 $BOAR mkrepo -d REPO >output.txt 2>&1 &&  { echo "Should not be possible to create dedup repo without dedup module"; exit 1; }
+grep "ERROR: Cannot create deduplicated repository: deduplication module is not installed" output.txt || { echo "Unexpected output"; exit 1; }
+rm output.txt || exit 1
+
+#
+# Test that a dedup repo cannot be committed to if the dedup module is
+# unavailable
+#
+
+$BOAR mkrepo -d $REPO || exit 1
+BOAR_DISABLE_DEDUP=1 $BOAR --repo="$REPO" mksession Session >output.txt 2>&1 && { 
+    echo "Should not be possible to create session in dedup repo without the dedup module"; exit 1; }
+
+cat >expected.txt <<EOF
+NOTICE: This repository requires the native deduplication module for writing -
+        only read operations can be performed.
+REPO USAGE ERROR: Repository is read-only
+!Finished in .* seconds
+EOF
+
+txtmatch.py expected.txt output.txt || {
+    echo "Creating session in dedup repo without dedup module gave unexpected error message"; exit 1; }
+
+$BOAR --repo="$REPO" mksession Session || exit 1
+
+BOAR_DISABLE_DEDUP=1 $BOAR --repo="$REPO" co Session >output.txt 2>&1 || { 
+    echo "Failed to check out session from dedup repo without dedup module"; exit 1; }
+
+cat >expected.txt <<EOF
+NOTICE: This repository requires the native deduplication module for writing -
+        only read operations can be performed.
+!Checking out to workdir .*
+!Finished in .* seconds
+EOF
+
+txtmatch.py expected.txt output.txt || {
+    echo "Checking out dedup repo without dedup module gave unexpected message"; exit 1; }
+
+( cd Session && echo "Tjohej" >a.txt && 
+    BOAR_DISABLE_DEDUP=1 $BOAR ci -q ) >output.txt 2>&1 && { 
+    echo "Committing to a dedup repo should fail"; exit 1; }
+
+cat >expected.txt <<EOF
+NOTICE: This repository requires the native deduplication module for writing -
+        only read operations can be performed.
+REPO USAGE ERROR: Repository is read-only
+!Finished in .* seconds
+EOF
+
+txtmatch.py expected.txt output.txt || {
+    echo "Committing in dedup repo without dedup module gave unexpected error message"; exit 1; }
+
+#
+# 
+#
+
+exit 0
