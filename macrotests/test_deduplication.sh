cc6082024d48fce054d910ec26450f186eda5d3d
Mats Ekberg <ekberg@gmail.com>
2013-03-21 22:58:13 +0100

    Test for truncated dedup repo

--- macrotests/test_deduplication.sh
+++ macrotests/test_deduplication.sh
@@ -34,6 +34,11 @@ cp $BIGFILE Session/a.bin || exit 1
     md5sum a.bin >manifest.md5 &&
     $BOAR ci -q ) || exit 1
 
+(cd Session && 
+    echo "Tjosan2" >>a.bin &&
+    md5sum a.bin >manifest.md5 &&
+    $BOAR ci -q ) || exit 1
+
 test -d "$REPO/recipes" || exit 1
 $BOAR --repo="$REPO" verify || { echo "Verify failed"; exit 1; }
 
@@ -161,6 +166,15 @@ rm -r "$REPO" "$CLONE" Session || exit 1
 $BOAR mkrepo -d "$REPO" || exit 1
 fill_repo
 touch $REPO/ENABLE_PERMANENT_ERASE || exit 1
-$BOAR --repo="$REPO" truncate || exit 1
+test -e $REPO/recipes/90d077e8f5d08222620ffc97bee8a19a.recipe || exit 1 # The previous one (should be culled)
+test -e $REPO/recipes/7efcbecf434ce1588570132fa61f53c6.recipe || exit 1 # The last one (should remain)
+
+$BOAR --repo="$REPO" truncate Session || exit 1
+
+test ! -e $REPO/recipes/90d077e8f5d08222620ffc97bee8a19a.recipe || { echo "Should have been removed by truncate"; exit 1; }
+test -e $REPO/recipes/7efcbecf434ce1588570132fa61f53c6.recipe || { echo "Should remain after truncate"; exit 1; }
+$BOAR --repo="$REPO" verify || { echo "Verify failed"; exit 1; }
+
+#exit 1
 
 exit 0
