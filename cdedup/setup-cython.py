fc3cfaf65b460d8eba67343d26001908ef11396d
Mats Ekberg <ekberg@gmail.com>
2013-04-09 21:37:58 +0200

    Removed unused code

--- cdedup/setup-cython.py
+++ cdedup/setup-cython.py
@@ -3,7 +3,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_modules = [Extension("rollingcs", 
-                         ["rollingcs.pyx", "rollsum.c", "intset.c", "bitfield.c", "circularbuffer.c", "blocksdb.c", "md5.c"],
+                         ["rollingcs.pyx", "rollsum.c", "intset.c", "bitfield.c", "circularbuffer.c", "blocksdb.c"],
                          extra_compile_args=["-static", "-O9", "-std=c99", "-Wall"],
                          extra_link_args=["sqlite3.o"],
                          )]
