c339f820a0e517a5d1402c4c264ffeba2bcc9adc
Mats Ekberg <ekberg@gmail.com>
2013-05-05 13:29:41 +0200

    Copyrights and minor fixes

--- cdedup/blocksdb.c
+++ cdedup/blocksdb.c
@@ -1,3 +1,17 @@
+// Copyright 2013 Mats Ekberg
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "stdlib.h"
 #include "stdio.h"
 #include "stdint.h"
@@ -91,12 +105,13 @@ static void blocksdb_log(const char* msg ) {
   fprintf(log, "%lld {PID %d} %s\n", time_in_mill, getpid(), msg); 
 }
 
+#ifdef ENABLE_BLOCKSDB_LOGGING
 static void trace_callback( void* udp, const char* sql ) { 
   char logmsg[1024];
   snprintf(logmsg, 1024, "[%s]", sql);
   blocksdb_log(logmsg);
 } 
-
+#endif
 
 static int hexchar2bin(const char c) {
   switch(c) {
--- cdedup/setup-cython.py
+++ cdedup/setup-cython.py
@@ -3,7 +3,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_modules = [Extension("cdedup", 
-                         ["cdedup.pyx", "rollsum.c", "intset.c", "bitfield.c", "circularbuffer.c", "blocksdb.c"],
+                         ["cdedup.pyx", "rollsum.c", "intset.c", "circularbuffer.c", "blocksdb.c"],
                          extra_compile_args=["-static", "-O2", "-std=c99", "-Wall"],
                          extra_link_args=["sqlite3.o"],
                          )]
